c455bd22d026e384fa4e7165dc5ef0a3938f246c
Josh Borrow <josh@joshborrow.com>
2021-09-21 09:10:09 -0400

    De-pydanticised config
    Signed-off-by: Josh Borrow <josh@joshborrow.com>

--- pageplot/config.py
+++ pageplot/config.py
@@ -4,44 +4,42 @@ full run of PlotPage.
 """
 
 from typing import Any, Dict, Optional
-from pydantic import BaseModel, validator
 
+import attr
 import unyt
 from matplotlib.pyplot import style
 
 from pageplot.configextension import ConfigExtension
 
 
-
-class GlobalConfig(BaseModel):
+@attr.s(auto_attribs=True)
+class GlobalConfig:
     stylesheet: Optional[str] = None
 
     unyt_matplotlib_support_enable: bool = True
-    unyt_label_style: str = "[]"
+    unyt_label_style: str = attr.ib(default="[]")
 
     extensions: Dict[str, Dict[str, Any]] = {}
 
-    @validator("stylesheet")
-    def load_stylesheet(cls, v):
-        style.use(v)
 
-    @validator("unyt_matplotlib_support_enable", always=True)
-    def enable_unyt_matplotlib_support(cls, v):
-        if v:
+    def __attrs_post_init__(self):
+        # Use the stylesheet
+        if self.stylesheet is not None:
+            style.use(self.stylesheet)
+
+        # Should we be using unyt support?
+        if self.unyt_matplotlib_support_enable:
             unyt.matplotlib_support.enable()
+            unyt.matplotlib_support.label_style = self.unyt_label_style
         else:
             unyt.matplotlib_support.disable()
 
-        return
 
-    @validator("unyt_label_style", always=True)
-    def set_unyt_label_style(cls, v):
-        if v not in ["[]", "()", "/"]:
+    @unyt_label_style.validator
+    def _check_valid_label_style(self, _, value):
+        if value not in ["[]", "()", "/"]:
             raise ValueError("You must choose one of [], (), or / for the unyt style.")
 
-        unyt.matplotlib_support.label_style = v
-
-        return
 
     def run_extensions(self, additional_extensions: Optional[Dict[str, ConfigExtension]] = None):
         """
@@ -57,6 +55,6 @@ class GlobalConfig(BaseModel):
         combined_extensions = {**built_in_config_extensions, **additional_extensions}
 
         for name, Extension in combined_extensions.items():
-            object.__setattr__(self, name, Extension(**self.extensions.get(name, {})))
+            setattr(self, name, Extension(**self.extensions.get(name, {})))
 
         return
