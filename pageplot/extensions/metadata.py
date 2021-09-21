eff9c5f4e2a17d360e161be756cae6a185584305
Josh Borrow <josh@joshborrow.com>
2021-09-20 20:32:45 -0400

    Added guards in metadata

--- pageplot/extensions/metadata.py
+++ pageplot/extensions/metadata.py
@@ -13,10 +13,12 @@ class MetadataExtension(PlotExtension):
     comment: Optional[str] = None
     title: Optional[str] = None
     caption: Optional[str] = None
+    section: Optional[str] = None
 
     def serialize(self):
         return {
-            "comment": self.comment,
-            "title": self.title,
-            "caption": self.caption,
+            "comment": self.comment if self.comment is not None else "",
+            "title": self.title if self.comment is not None else "",
+            "caption": self.caption if self.caption is not None else "",
+            "section": self.section if self.section is not None else "",
         }
